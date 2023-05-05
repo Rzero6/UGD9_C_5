254872f59552ce34d5945a5f47fd999f4ef38eb5
Msi Modern 14 <wibowolala@gmail.com>
2023-05-05 17:08:21 +0700

    Merge origin/master
    Conflicts:
    	src/exception/ExceptionId.java

---
+++ src/Exception/JenisKendaraanException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exception;
+
+/**
+ *
+ * @author Msi Modern 14
+ */
+public class JenisKendaraanException {
+    public String message(){
+        return "Jenis Kendaraan Hanya Mobil / Motor [!]";
+    }
+}
